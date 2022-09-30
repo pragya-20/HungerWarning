de63fe8a18d8a2b154deaef4e2dc04c425588497
Pragya Bhardwaj <pragyabhardwaj96@gmil.com>
2022-10-01 00:21:49 +0530

    Add Main Screen

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginSignUp from './screens/LoginSignUp';
 import MainScreen from './screens/MainScreen';
+import auth from '@react-native-firebase/auth';
+// import {useState} from 'react';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  // const [user, setUser] = useState(null);
+
+  // function isStateChanged(loggedUser) {
+  //   console.log('UserRceived', loggedUser);
+  //   setUser(loggedUser);
+  // }
+
+  // useEffect(() => {
+  //   console.log('useEffect called');
+  //   const subscriber = auth().onAuthStateChanged(isStateChanged);
+  //   return subscriber;
+  // }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{headerShown: false}}
-        initialRouteName="LogIn">
+        initialRouteName="MainScreen">
         <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
         <Stack.Screen name="LogIn" component={LoginSignUp}></Stack.Screen>
         <Stack.Screen name="MainScreen" component={MainScreen}></Stack.Screen>
